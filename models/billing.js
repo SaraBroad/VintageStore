b64e01ebcb02bb7629edb1c45afb0c16dee99135
Sara Broad <saraebroad@gmail.com>
2018-06-24 15:07:41 -0400

    billing model

---
+++ models/billing.js
@@ -0,0 +1,45 @@
+var Sequelize = require("sequelize");
+//need connection to db
+
+module.exports = function (sequelize, DataTypes) {
+    var Billing = sequelize.define("billing", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        addressOne: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        addressTwo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        zip: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            isEmail: true
+        }
+    });
+    return Billing;
+};
