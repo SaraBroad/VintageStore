77478b7c14839073b46f2d742ccda07c3e670c96
Sara Broad <saraebroad@gmail.com>
2018-06-26 13:25:46 -0400

    checkout form

--- client/src/components/CheckoutCard/CheckoutCard.js
+++ client/src/components/CheckoutCard/CheckoutCard.js
@@ -10,52 +10,47 @@ const CheckoutCard = props => (
         <h3>Shipping Information</h3>
             <label htmlFor="cust-name">Name</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="cust-name"
                 type="text"
-                className="form-control"
                 placeholder="Name"
                 id="cust-name"
             />
            <label htmlFor="cust-address">Street Address 1</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="cust-address"
                 type="text"
-                className="form-control"
                 placeholder="Street Address 1"
                 id="cust-address"
             />
            <label htmlFor="cust-addresstwo">Street Address 2</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="cust-addresstwo"
                 type="text"
-                className="form-control"
                 placeholder="Street Address 2"
                 id="cust-addresstwo"
             />
             <label htmlFor="city">City</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="city"
                 type="text"
-                className="form-control"
                 placeholder="City"
                 id="city"
             />
             <label htmlFor="state">State</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="state"
                 type="text"
-                className="form-control"
                 placeholder="State"
                 id="state"
             />
            <label htmlFor="zip">Zip Code</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="zip"
                 type="text"
                 className="form-control"
@@ -64,19 +59,17 @@ const CheckoutCard = props => (
             />
             <label htmlFor="phone">Phone Number</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="phone"
                 type="number"
-                className="form-control"
                 placeholder="Phone Number"
                 id="phone"
             />
            <label htmlFor="email">E-mail</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="email"
                 type="email"
-                className="form-control"
                 placeholder="E-mail"
                 id="email"
             />
@@ -86,70 +79,63 @@ const CheckoutCard = props => (
          <h3>Billing Information</h3>
          <label htmlFor="billcust-name">Name</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="billcust-name"
                 type="text"
-                className="form-control"
                 placeholder="Name"
                 id="billcust-name"
             />
            <label htmlFor="billcust-address">Street Address 1</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="billcust-address"
                 type="text"
-                className="form-control"
                 placeholder="Street Address 1"
                 id="billcust-address"
             />
            <label htmlFor="billcust-address2">Street Address 2</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="billcust-addresstwo"
                 type="text"
-                className="form-control"
                 placeholder="Street Address 2"
                 id="billcust-addresstwo"
             />
             <label htmlFor="bill-city">City</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="bill-city"
                 type="text"
-                className="form-control"
                 placeholder="City"
                 id="bill-city"
             />
            <label htmlFor="bill-state">State</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="bill-state"
                 type="text"
-                className="form-control"
                 placeholder="State"
                 id="billstate"
             />
             <label htmlFor="bill-zip">Zip Code</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="bill-zip"
                 type="text"
-                className="form-control"
                 placeholder="Zip Code"
                 id="billzip"
             />
             <label htmlFor="bill-phone">Phone Number</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="bill-phone"
                 type="number"
-                className="form-control"
                 placeholder="Phone Number"
                 id="bill-phone"
             />
             <label htmlFor="bill-email">Email</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="bill-email"
                 type="email"
                 className="form-control"
@@ -162,34 +148,31 @@ const CheckoutCard = props => (
         <h3>Payment Information</h3>
         <label htmlFor="card-name">Name on card</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="card-name"
                 type="text"
-                className="form-control"
                 placeholder="Name on card"
                 id="card-name"
             />
         <label htmlFor="card-number">Enter card number</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="card-number"
-                type="number"
-                className="form-control"
+                type="text"
                 placeholder="Card Number"
                 id="card-number"
             />
          <label htmlFor="cvv">CVV</label>
             <input
-                className="custinput-bar"
+                className="custinput-bar form-control"
                 name="cvv"
-                type="number"
-                className="form-control"
+                type="text"
                 placeholder="CVV"
                 id="cvv"
             />
             {/* Expiration */}
         
-
+            <button type="button" className="btn btn-primary submit">Submit</button>
         </div>
 
 
