f828c245dae1251127cd6d5f4a603919273b38a1
Sara Broad <saraebroad@gmail.com>
2018-06-26 15:39:56 -0400

    FAQs

--- client/src/pages/FAQs.js
+++ client/src/pages/FAQs.js
@@ -1,16 +1,14 @@
-
-// import ContactCard from "../components/ContactCard";
-
-// class Contact extends Component {
-//     render () {
-//         return (
-//             <div>
-//                 <ContactCard />
-//             </div>
-//         )
-//     }
-// }
-
-// export default Contact;
-
 import React, { Component } from 'react';
+import FaqCard from "../components/FaqCard";
+
+class FAQs extends Component {
+    render () {
+        return (
+            <div>
+                <FaqCard />
+            </div>
+        )
+    }
+}
+
+export default FAQs;
