79cc06594246830a6799b13ba52433547ebb1dcb
Sara Broad <saraebroad@gmail.com>
2018-06-30 18:44:04 -0400

    cart product table

--- models/cart.js
+++ models/cart.js
@@ -4,16 +4,14 @@ module.exports = function (sequelize, DataTypes) {
     var Cart = sequelize.define("Cart", {
 
     })
-    
+
     Cart.associate = function (models) {
  
-    //     Cart.belongsToMany(models.Products, {
-    //         through: "CartProduct",
-    //         as: "CartCartProduct",
-    //         foreignKey: "cartId"
-    //     })
-    //     // ,
-
+        Cart.belongsToMany(models.Products, {
+            through: "CartProduct",
+            as: "CartCartProduct",
+            foreignKey: "cartId"
+        }),
         Cart.belongsTo(models.Customer, {
             foreignKey: {
                 allowNull: false
