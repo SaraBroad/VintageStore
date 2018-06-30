f3ae7f2e64e1e42da8ff1d0e918a626da6eaa9a0
Sara Broad <saraebroad@gmail.com>
2018-06-30 18:37:52 -0400

    working purchase history model

--- models/cart.js
+++ models/cart.js
@@ -1,18 +1,23 @@
-var Sequelize = require("sequelize");
+var Sequelize = require('sequelize');
 
 module.exports = function (sequelize, DataTypes) {
     var Cart = sequelize.define("Cart", {
 
+    })
+    // Cart.associate = function (models) {
+ 
+    //     Cart.belongsToMany(models.Products, {
+    //         through: "CartProduct",
+    //         as: "CartCartProduct",
+    //         foreignKey: "cartId"
+    //     })
+    //     // ,
 
-
-    });
-
-    Cart.associate = function(models) {
-        Cart.belongsTo(models.Customer, {
-            foreignKey: {
-                allowNull: false
-            }
-        })
-    }
+    //     // Cart.belongsTo(models.Customer, {
+    //     //     foreignKey: {
+    //     //         allowNull: false
+    //     //     }
+    //     // });
+    // }
     return Cart;
 };
---
+++ models/purchaseproducts.js
@@ -0,0 +1,19 @@
+var Sequelize = require("sequelize");
+
+module.exports = function (sequelize, DataTypes) {
+
+    var PurchasedItems = sequelize.define("PurchasedItems", {
+        //not sure what goes here yet since all we need everything else is either auto-created or foreign
+
+    })
+
+    PurchasedItems.associate = function (models) {
+        PurchasedItems.belongsTo(models.Products, {
+            foreignKey: {
+                allowNull: false
+            }
+        })
+    }
+
+    return PurchasedItems;
+};
