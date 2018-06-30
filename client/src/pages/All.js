322fb4c884cb24b1f0027aebb75aa1bc6d7ad12c
Drita <ddogjani@gmail.com>
2018-06-30 10:44:18 -0400

    frontend code

---
+++ client/src/pages/All.js
@@ -0,0 +1,22 @@
+// import React, { Component } from "react";
+// import Product from "../components/Product";
+// import products from "../public/products.json"
+
+
+// class All extends Component {
+//         // Set this.state
+//         state = {
+//           products
+//         }
+        
+//         render() {
+
+//             return (
+//             <div>
+//                 <Product/>
+//             </div>
+               
+//             )
+//         }
+// }
+// export default All;
