bc7e3fe7afa506e9985678485e48bf68436517d1
Sara Broad <saraebroad@gmail.com>
2018-07-17 20:20:43 -0400

    merge fixes

--- controllers/cartProductController.js
+++ controllers/cartProductController.js
@@ -4,13 +4,11 @@ const db = require("../models")
 module.exports = function(app) {
 
     app.post("/api/cartProduct", function (req, res) {
-        console.log(req.body)
+        // console.log(req.body)
 
-        db.CartProduct.update({
-            where: {
-            productId: req.body.id,
-            cartId: req.body.id
-            }
+        db.CartProduct.create({
+            productId: req.body.productId,
+            cartId: req.body.cartId
         })
         .then(function (dbCartProduct) {
             // console.log(dbCartProduct)
@@ -20,9 +18,9 @@ module.exports = function(app) {
 
     app.get("/api/allCartProducts", function (req, res) {
 
-        db.CartProduct.findAll({}).then(function (dbCartProduct) {
+        db.CartProduct.findAll({}).then(function (dbCartProducts) {
             // console.log(dbCartProducts);
-            res.json(dbCartProduct);
+            res.json(dbCartProducts);
         })
         .catch( err => res.json(err) );
     })
