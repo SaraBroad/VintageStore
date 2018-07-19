7cfcf064afb156437e368d79c69b332e26b1ce50
Adeline Johnson <groblewski.adeline@gmail.com>
2018-07-19 17:08:46 -0400

    updated server.js

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ require('./routes/api/passport_routes.js')(app);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 
 if(process.env.NODE_ENV === 'production') {
