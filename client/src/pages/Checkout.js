1553bcc05890fae9f6df72505e4bb40d73eedc2e
Adeline Johnson <groblewski.adeline@gmail.com>
2018-07-19 08:40:14 -0400

    still heroku

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Elements, StripeProvider} from 'react-stripe-elements';
 import CheckoutForm from '../components/Checkoutform';
-import STRIPE_PUBLISHABLE from '../constants/stripe';
+
 
 class Checkout extends Component {
   render() {
